b3d34602e05cea2d31ff3bed8d137b83fd1782bf
EvanShabarovskiy <ivanshabarovskij@gmail.com>
2025-10-21 10:47:18 +0300

    Added headless mode to DriverFactory. Enabled by HEADLESS_MODE field in the same class

--- src/test/java/org/example/utils/DriverFactory.java
+++ src/test/java/org/example/utils/DriverFactory.java
@@ -3,16 +3,38 @@ package org.example.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
+    //---------SETTING---------
+    private static final boolean HEADLESS_MODE = true;
+    //-------------------------
+
     public static WebDriver getDriver() {
         if (driver.get() == null) {
-            WebDriverManager.chromedriver().setup();
-            driver.set(new ChromeDriver());
+            driver.set(createDriver());
         }
         return driver.get();
+    }
+
+    private static WebDriver createDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        if (HEADLESS_MODE) {
+            options.addArguments("--headless=new");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            System.out.println("🧠 Running in headless mode");
+        } else {
+            System.out.println("🪟 Running in normal (UI) mode");
+        }
+
+        return new ChromeDriver(options);
     }
 
     public static void quitDriver() {
